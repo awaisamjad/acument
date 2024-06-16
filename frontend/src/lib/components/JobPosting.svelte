559824b1b0a877b8d85475df5acc3eaa64da44bc
Awais Amjad <awaisamjad08@gmail.com>
2024-06-16 22:58:45 +0100

    Fixed Image and Font paths. Animations still dont work

--- frontend/src/lib/components/JobPosting.svelte
+++ frontend/src/lib/components/JobPosting.svelte
@@ -9,28 +9,30 @@
 	let rolesAndResponsibilities = '';
 
 	try {
-		fetch('http://localhost:1337/api/jobs', {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		})
-			.then((response) => response.json())
-			.then((json_response) => {
-				console.log("Response JSON", json_response);
-				console.log("Response JSON Attributes", json_response.data[0].attributes.title);
+		onMount(async () => {
+			await fetch('http://localhost:1337/api/jobs', {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			})
+				.then((response) => response.json())
+				.then((json_response) => {
+					console.log('Response JSON', json_response);
+					console.log('Response JSON Attributes', json_response.data[0].attributes.title);
 
-				const job = json_response.data[0];
-				const attributes = job.attributes;
+					const job = json_response.data[0];
+					const attributes = job.attributes;
 
-				title = attributes.title;
-				category = attributes.category;
-				date = attributes.date;
-				location = attributes.location;
-				rolesAndResponsibilities = attributes.roles_and_responsibilities;
+					title = attributes.title;
+					category = attributes.category;
+					date = attributes.date;
+					location = attributes.location;
+					rolesAndResponsibilities = attributes.roles_and_responsibilities;
 
-				console.log("title: ", title);
-			});
+					console.log('title: ', title);
+				});
+		});
 	} catch (error) {
 		console.log('Job API Error');
 		console.error(error);
@@ -82,38 +84,43 @@
 </div>
 
 <style lang="scss">
-    body {
-        font-family: Arial, sans-serif;
-        color: #333;
-        background-color: #f4f4f4;
-        margin: 0;
-        padding: 0;
-    }
+	body {
+		font-family: Arial, sans-serif;
+		color: #333;
+		background-color: #f4f4f4;
+		margin: 0;
+		padding: 0;
+	}
 
-    div {
-        &.date {
-            p {
-                color: #ffbbbb;
-                font-size: 1.2em;
-                font-weight: bold;
-            }
-        }
-    }
+	div {
+		&.date {
+			p {
+				color: #ffbbbb;
+				font-size: 1.2em;
+				font-weight: bold;
+			}
+		}
+	}
 
-    h1, h2, h3, h4, h5, h6 {
-        color: #444;
-    }
+	h1,
+	h2,
+	h3,
+	h4,
+	h5,
+	h6 {
+		color: #444;
+	}
 
-    p {
-        line-height: 1.6em;
-    }
+	p {
+		line-height: 1.6em;
+	}
 
-    a {
-        color: #06c;
-        text-decoration: none;
+	a {
+		color: #06c;
+		text-decoration: none;
 
-        &:hover {
-            text-decoration: underline;
-        }
-    }
+		&:hover {
+			text-decoration: underline;
+		}
+	}
 </style>
